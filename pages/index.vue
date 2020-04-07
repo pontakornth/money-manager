a5183778a15061afecd064323960339b785e8e8e
pontakornth <pontakorn61@gmail.com>
2020-04-07 19:40:39 +0700

    content: Edit caption

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,20 @@
 <template>
   <section class="section">
     <div class="columns is-mobile">
-      <card title="Free" icon="github-circle">
-        Open source on
-        <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
+      <card title="Free" icon="piggy-bank">
+        Managing money easy way
       </card>
 
-      <card title="Responsive" icon="cellphone-link">
-        <b class="has-text-grey">
-          Every
-        </b>
-        component is responsive
+      <card title="Responsive" icon="book">
+        Easy planning
       </card>
 
-      <card title="Modern" icon="alert-decagram">
-        Built with
-        <a href="https://vuejs.org/">
-          Vue.js
-        </a>
-        and
-        <a href="http://bulma.io/">
-          Bulma
-        </a>
+      <card title="Modern" pack="mdi" icon="shield-account">
+        Care about your security
       </card>
 
-      <card title="Lightweight" icon="arrange-bring-to-front">
-        No other internal dependency
+      <card title="Lightweight" icon="heart">
+        Built with love
       </card>
     </div>
   </section>
