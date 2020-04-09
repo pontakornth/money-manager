487f34bbb5d69a57db08b630d4d6ac9b91951a30
pontakornth <pontakorn61@gmail.com>
2020-04-09 19:36:03 +0700

    styling: unify select in one line

--- pages/ledger.vue
+++ pages/ledger.vue
@@ -5,22 +5,30 @@
       <b-field label="รายการ">
         <b-input type="text" name="title" placeholder="กะเพราหมูสับ" />
       </b-field>
-      <b-field label="ประเภท">
-        <b-select>
-          <option value="income">รายรับ</option>
-          <option value="income">รายจ่าย</option>
-        </b-select>
-      </b-field>
-      <b-field label="ประเภทรายการ">
-        <b-select>
-          <option value="อาหาร">อาหาร</option>
-          <option value="เครื่องแต่งกาย">เครื่องแต่งกาย</option>
-          <option value="การศึกษา">การศึกษา</option>
-        </b-select>
-      </b-field>
-      <b-field label="จำนวนเงิน">
-        <b-input type="number" />
-      </b-field>
+      <div class="columns is-gapless">
+        <div class="column is-4">
+          <b-field label="ประเภท">
+            <b-select>
+              <option value="income">รายรับ</option>
+              <option value="income">รายจ่าย</option>
+            </b-select>
+          </b-field>
+        </div>
+        <div class="column is-4">
+          <b-field label="ประเภทรายการ">
+            <b-select>
+              <option value="อาหาร">อาหาร</option>
+              <option value="เครื่องแต่งกาย">เครื่องแต่งกาย</option>
+              <option value="การศึกษา">การศึกษา</option>
+            </b-select>
+          </b-field>
+        </div>
+        <div class="column is-4">
+          <b-field label="จำนวนเงิน">
+            <b-input type="number" step="0.01" />
+          </b-field>
+        </div>
+      </div>
     </form>
   </div>
 </template>
