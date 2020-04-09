fd37a58733e0f8757dea08731116a260ba32ddcd
pontakornth <pontakorn61@gmail.com>
2020-04-09 11:14:53 +0700

    feat: Add mock ledger page

---
+++ pages/ledger.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1 class="title is-1">ไม่มีรายการ</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component({
+  layout: 'account'
+})
+class Ledger extends Vue {}
+export default Ledger
+</script>
