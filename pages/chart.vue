17d51f00aff4fdd934b310de05628890fbe28b25
pontakornth <pontakorn61@gmail.com>
2020-04-10 20:35:01 +0700

    feat: Add chart page

---
+++ pages/chart.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="padded">
+    <h1 class="title is-1">ข้อมูลการใช้เงิน</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component({
+  layout: 'account'
+})
+class Chart extends Vue {}
+export default Chart
+</script>
+
+<style lang="scss" scoped>
+.ledger-form {
+  width: 50%;
+}
+.padded {
+  padding: 1rem;
+}
+.mt-1 {
+  margin-top: 1rem;
+}
+</style>
