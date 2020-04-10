682a607529c28b849b7673d269b9a2cc1177f31f
pontakornth <pontakorn61@gmail.com>
2020-04-10 13:59:10 +0700

    feat: Add ledger table component

---
+++ components/LedgerTable.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-table
+    striped
+    :mobile-cards="false"
+    :data="transactions"
+    :columns="columns"
+  ></b-table>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+@Component
+class LedgerTable extends Vue {
+  @Prop({ type: Array }) transactions!: Array<any>
+  columns = [
+    {
+      field: 'title',
+      label: 'รายการ'
+    },
+    {
+      field: 'income',
+      label: 'รายรับ',
+      numeric: true
+    },
+    {
+      field: 'expense',
+      label: 'รายจ่าย',
+      numeric: true
+    }
+  ]
+}
+export default LedgerTable
+</script>
