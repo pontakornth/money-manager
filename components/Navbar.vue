164df505d3478499fc3abe303a4c00d5b5915d97
pontakornth <pontakorn61@gmail.com>
2020-04-10 14:10:05 +0700

    styling: Make navbar a serparate component

---
+++ components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-navbar type="is-primary">
+    <template slot="brand">
+      <b-navbar-item tag="nuxt-link" to="/">
+        <b>Money Manager</b>
+      </b-navbar-item>
+    </template>
+    <template slot="start">
+      <b-navbar-item tag="nuxt-link" to="/">
+        หน้าหลัก
+      </b-navbar-item>
+      <b-navbar-item href="#">
+        วิธีการใช้งาน
+      </b-navbar-item>
+      <b-navbar-dropdown label="ตั้งค่า">
+        <b-navbar-item href="#">
+          บัญชี
+        </b-navbar-item>
+        <b-navbar-item href="#">
+          ตั้งค่าอื่น ๆ
+        </b-navbar-item>
+      </b-navbar-dropdown>
+    </template>
+
+    <template slot="end">
+      <b-navbar-item tag="div">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>สมัครสมาชิก</strong>
+          </a>
+          <nuxt-link to="/login" class="button is-light">
+            เข้าสู่ระบบ
+          </nuxt-link>
+        </div>
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
