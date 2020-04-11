76a93dafc101bbca2a731b1d5b1f5bbd79dbc32a
pontakornth <pontakorn61@gmail.com>
2020-04-11 14:05:52 +0700

    feat: Add Chart page (mock)

---
+++ components/charts/DonutChart.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { Doughnut, mixins } from 'vue-chartjs'
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+const { reactiveProp } = mixins
+@Component({
+  extends: Doughnut,
+  mixins: [reactiveProp]
+})
+class DonutChart extends Vue {
+  public renderChart!: (chartData: any, options: any) => void
+  @Prop({ type: Object }) chartData!: any
+  @Prop({ type: Object }) options!: any
+  mounted() {
+    this.renderChart(this.chartData, this.options)
+  }
+}
+export default DonutChart
+</script>
--- pages/chart.vue
+++ pages/chart.vue
@@ -1,16 +1,57 @@
 <template>
   <div class="padded">
     <h1 class="title is-1">ข้อมูลการใช้เงิน</h1>
+    <div class="columns is-multiline">
+      <div class="column is-half">
+        <DonutChart :chart-data="expenseData" />
+      </div>
+      <div class="column is-half">
+        <DonutChart :chart-data="incomeData" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
-
+import DonutChart from '~/components/charts/DonutChart.vue'
 @Component({
-  layout: 'account'
+  layout: 'account',
+  components: {
+    DonutChart
+  }
 })
-class Chart extends Vue {}
+class Chart extends Vue {
+  expenseData = {
+    labels: ['อาหาร', 'การศึกษา', 'บันเทิง'],
+    datasets: [
+      {
+        labels: '# Money spended',
+        data: [200, 500, 200],
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+          'rgb(255, 206, 86)'
+        ]
+      }
+    ]
+  }
+
+  incomeData = {
+    labels: ['ค่าขนม', 'ค่าจ้าง', 'ของขวัญ'],
+    datasets: [
+      {
+        labels: '# Money Recieved',
+        data: [1500, 2500, 200],
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+          'rgb(255, 206, 86)'
+        ]
+      }
+    ]
+  }
+}
 export default Chart
 </script>
 
