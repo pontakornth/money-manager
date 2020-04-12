46b67f3caa09ea362dbfbf6eefa3f65c8b5fc431
pontakornth <pontakorn61@gmail.com>
2020-04-12 11:47:51 +0700

    feat: Add empty planner

---
+++ pages/planner.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="padded">
+    <h1 class="title is-1">วางแผนการออม</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component({
+  layout: 'account'
+})
+class Planner extends Vue {}
+export default Planner
+</script>
+
+<style lang="scss" scoped>
+.ledger-form {
+  width: 50%;
+}
+.padded {
+  padding: 1rem;
+}
+.mt-1 {
+  margin-top: 1rem;
+}
+</style>
