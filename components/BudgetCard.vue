bc0687bfbfaafef0d6a4d2a5377d95a9322f8190
pontakornth <pontakorn61@gmail.com>
2020-04-12 14:53:12 +0700

    feat: Add mock budget planner page

---
+++ components/BudgetCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card">
+    <header class="card-header">
+      <p class="card-header-title">
+        {{ title }}
+      </p>
+    </header>
+    <div class="card-content">
+      <div class="content">
+        <p>ราคา: {{ price }}</p>
+        <p>เก็บเงินไว้ : {{ current }}</p>
+        <p>เหลือ : {{ left }}</p>
+      </div>
+      <div class="buttons">
+        <b-button type="is-success">เพิ่มเงิน</b-button>
+        <b-button type="is-warning">ถอนเงิน</b-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+@Component
+class BudgetCard extends Vue {
+  @Prop({ type: String }) title!: string
+  @Prop({ type: Number }) price!: number
+  @Prop({ type: Number }) current!: number
+  get left() {
+    return this.price - this.current
+  }
+}
+export default BudgetCard
+</script>
--- pages/planner.vue
+++ pages/planner.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="padded">
     <h1 class="title is-1">วางแผนการออม</h1>
+    <div class="columns is-multiline">
+      <div class="column is-4">
+        <BudgetCard title="Nintendo Hub" :price="1200" :current="500" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
-
+import BudgetCard from '~/components/BudgetCard.vue'
 @Component({
-  layout: 'account'
+  layout: 'account',
+  components: {
+    BudgetCard
+  }
 })
 class Planner extends Vue {}
 export default Planner
